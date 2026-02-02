67fd9cba4b2cca6eaf24be44fa5e2aa947162674
yvngnappy <156217510+yvngnappy@users.noreply.github.com>
2026-02-01 23:05:46 -0500

    Added hobby to show a fun fact about myself

--- StudentInfo.java
+++ StudentInfo.java
@@ -5,6 +5,7 @@ public class StudentInfo {
         String major = "Information Technology";
         String email = "james_emmanuel@students.columbusstate.edu";
         String hometown = "Brooklyn, NY";
+        String hobby = "Skateboarding";
         int graduationYear = 2025;
 
         System.out.println("==========================================");
@@ -13,6 +14,7 @@ public class StudentInfo {
         System.out.println("Name: " + name);
         System.out.println("Email: " + email);
         System.out.println("Hometown: " + hometown);
+        System.out.println("Favorite Hobby: " + hobby);
         System.out.println("Major: " + major);
         System.out.println("Graduation Year: " + graduationYear);
         System.out.println("==========================================");
